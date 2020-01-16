293821b512300074af88b2cad53108bc91471b31
vivianachoque <vivianaachoque@gmail.com>
2020-01-16 16:01:01 -0300

    Resolvi ejercicio #1,2

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -6,11 +6,18 @@ const inputNombre = document.querySelector('#input-nombre');
 function recibirNombre(event) {
   event.preventDefault();
   console.log(event);
+  alert("Formulario enviado correctamente")
 }
 
-function interceptarNombre(event) {
-  console.log(event.target.value);
+function interceptarNombre(event){
+  const nombre = event.value;
+  if(nombre.length <=4 || nombre.length>50){
+    console.log(" debe tener entre 5 y 50 caracteres");
+    } else{
+      console.log("es valido");
+    }
 }
+
 
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
